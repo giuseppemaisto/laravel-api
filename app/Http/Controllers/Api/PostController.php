87cb0ac9c5268a9eb4672284d7ab09a6d498a961
giuseppemaisto <gmaisto98@gmail.com>
2023-03-13 17:05:46 +0100

    corretto post controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
         if($post){
             return response()->json([
                 'success' => true,
-                'result' => $post
+                'post' => $post
             ]);
         }
         else{
